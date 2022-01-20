cc997ea681129c92772ba47829607e999c14d64e
Anthea Ip <antheaip964@gmail.com>
2022-01-20 10:00:35 -0500

    Saving

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import GettingGraphQLData from "./components/GettingGraphQLData";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/person"
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <GettingGraphQLData />
+  </ApolloProvider>,
+   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
